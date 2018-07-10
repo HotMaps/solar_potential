580a51c9ca7a2ea0ad2b6a5799adc36c0ad58de0
pietro <peter.zamb@gmail.com>
2018-07-10 18:05:32 +0200

    Fix python3 print function

--- cm/app/api_v1/transactions.py
+++ cm/app/api_v1/transactions.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import json
 import os
 import uuid
@@ -6,7 +8,7 @@ import requests
 from flask import (abort, flash, g, jsonify, request, send_from_directory,
                    url_for)
 
-from calculation_module import calculation
+from .calculation_module import calculation
 
 from . import api
 from .. import CM_NAME, SIGNATURE, URL_MAIN_WEBSERVICE
@@ -40,15 +42,15 @@ def register():
             results: {"response": {"category": "Buildings", "cm_name": "calculation_module_1", "layers_needed": ["heat_density_tot"], "cm_description": "this computation module allows to ....", "cm_url": "http://127.0.0.1:5002/", "cm_id": 1, "inputs_calculation_module": [{"input_min": 1, "input_value": 1, "input_unit": "none", "input_name": "Reduction factor", "cm_id": 1, "input_type": "input", "input_parameter_name": "reduction_factor", "input_max": 10}, {"input_min": 10, "input_value": 50, "input_unit": "", "input_name": "Blablabla", "cm_id": 1, "input_type": "range", "input_parameter_name": "bla", "input_max": 1000}]}}
              """
 
-    print 'CM will register '
+    print('CM will register ')
 
     base_url = request.base_url.replace("computation-module/register/", "")
-    print 'CM base_url register '
+    print('CM base_url register ')
     signature_final = SIGNATURE
     signature_final["cm_url"] = base_url
 
     payload = json.dumps(signature_final)
-    print payload
+    print(payload)
     registerCM(payload)
     return json.dumps({'response': payload})
 
@@ -58,16 +60,16 @@ def registerCM(data):
     res = requests.post(URL_MAIN_WEBSERVICE + 'api/cm/register/', data=data, headers=headers)
     reponse = res.text
     status_code = res.status_code
-    print 'res.json() ', res.json()
-    print 'status_code ', status_code
-    print 'reponse ', reponse
+    print('res.json() ', res.json())
+    print('status_code ', status_code)
+    print('reponse ', reponse)
     return requests
 
 
 def savefile(filename, url):
-    print url
+    print(url)
     r = requests.get(url, stream=True)
-    print r.status_code
+    print(r.status_code)
     if r.status_code == 200:
         path = os.path.join(UPLOAD_DIRECTORY, filename)
         with open(path, 'wb') as f:
@@ -108,7 +110,7 @@ def compute():
             results: {"response": {"category": "Buildings", "cm_name": "calculation_module_1", "layers_needed": ["heat_density_tot"], "cm_description": "this computation module allows to ....", "cm_url": "http://127.0.0.1:5002/", "cm_id": 1, "inputs_calculation_module": [{"input_min": 1, "input_value": 1, "input_unit": "none", "input_name": "Reduction factor", "cm_id": 1, "input_type": "input", "input_parameter_name": "reduction_factor", "input_max": 10}, {"input_min": 10, "input_value": 50, "input_unit": "", "input_name": "Blablabla", "cm_id": 1, "input_type": "range", "input_parameter_name": "bla", "input_max": 1000}]}}
              """
 
-    print 'CM will Compute '
+    print('CM will Compute ')
     #import ipdb; ipdb.set_trace()
     data = request.get_json()
     url_file = data["url_file"]
@@ -116,7 +118,7 @@ def compute():
     # part to modify from the CM rpovider
     # parameters needed from the CM
     reduction_factor = data["reduction_factor"]
-    print 'reduction_factor ', reduction_factor
+    print('reduction_factor ', reduction_factor)
     file_path = savefile(filename, url_file)
     filename = str(uuid.uuid4()) + '.tif'
     path_final = UPLOAD_DIRECTORY + '/' + filename
@@ -125,7 +127,7 @@ def compute():
     base_url = request.base_url.replace("compute", "files")
     # 1.2.1  url for downloading raster
     url_download_raster = base_url + filename
-    print 'indicator {}'.format(indicator)
+    print('indicator {}'.format(indicator))
     response = {
         'indicators': [{
             'indicator_name': 'heat_density',
@@ -148,4 +150,4 @@ def call_calculation_module(file_name, factor=2, directory=UPLOAD_DIRECTORY):
 # Delete CM from main API
 @api.route('/unregister/' + CM_NAME, methods=['DELETE'])
 def unregister_cm(cm_name):
-    print 'request for deleting CM'
+    print('request for deleting CM')

04ac914809404c3e3d9bee660ccfb8ef25af010b
ggaregnani <giulia.garegnani@eurac.edu>
2018-10-12 10:10:47 +0200

    add test by reading the signature in constat.py

--- cm/tests/tests.py
+++ cm/tests/tests.py
@@ -4,6 +4,8 @@ from app import create_app
 import os.path
 from shutil import copyfile
 from .test_client import TestClient
+from app.constant import INPUTS_CALCULATION_MODULE
+
 UPLOAD_DIRECTORY = '/var/hotmaps/cm_files_uploaded'
 
 if not os.path.exists(UPLOAD_DIRECTORY):
@@ -12,7 +14,6 @@ if not os.path.exists(UPLOAD_DIRECTORY):
 
 
 class TestAPI(unittest.TestCase):
-
 
     def setUp(self):
         self.app = create_app(os.environ.get('FLASK_CONFIG', 'development'))
@@ -25,24 +26,23 @@ class TestAPI(unittest.TestCase):
 
         self.ctx.pop()
 
-
     def test_compute(self):
+        # load the raster test file and the default values
+        # from app.constant
         raster_file_path = 'tests/data/raster_for_test.tif'
         # simulate copy from HTAPI to CM
         save_path = UPLOAD_DIRECTORY+"/raster_for_test.tif"
         copyfile(raster_file_path, save_path)
-
         inputs_raster_selection = {}
         inputs_parameter_selection = {}
-        inputs_raster_selection["heat_tot_curr_density"]  = save_path
-        inputs_parameter_selection["reduction_factor"] = 2
+        inputs_raster_selection["solar_optimal_total"]  = save_path
+        for inp in INPUTS_CALCULATION_MODULE:
+            inputs_parameter_selection[inp['input_parameter_name']]  = inp['input_value']
         # register the calculation module a
         payload = {"inputs_raster_selection": inputs_raster_selection,
                    "inputs_parameter_selection": inputs_parameter_selection}
 
-
-        rv, json = self.client.post('computation-module/compute/', data=payload)
+        rv, json = self.client.post('computation-module/compute/',
+                                    data=payload)
 
         self.assertTrue(rv.status_code == 200)
-
-

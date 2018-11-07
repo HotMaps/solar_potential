6e37444459c9dc42164b7b9f35635a6a41ec843a
lesly houndole <lesly.houndole@crem.ch>
2018-11-07 14:33:30 +0100

    test solar potentail

--- cm/run_all_local_terminal.sh
+++ cm/run_all_local_terminal.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 
-sudo gnome-terminal -e "python consumer_cm_alive.py" --title="consumer_cm_alive"
+sudo gnome-terminal -e "python consumer_cm_alive.py" --title="solar_potential: consumer_cm_alive"
 
-sudo gnome-terminal -e "python run.py" --title="run CM"
+sudo gnome-terminal -e "python run.py" --title="solar_potential: run CM"
 
-sudo gnome-terminal -e "python consumer_cm_compute.py" --title="consumer_cm_compute_solar_potential"
+sudo gnome-terminal -e "python consumer_cm_compute.py" --title="solar_potential: consumer_cm_compute_solar_potential"
 
-sudo gnome-terminal -e "python3 register_cm.py" --title="register_cm"
+sudo gnome-terminal -e "python3 register_cm.py" --title="solar_potential: register_cm"
 
 
 

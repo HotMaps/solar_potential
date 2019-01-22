cb7c6a241874ec40a52f912da0666f934fd8e965
lesly houndole <lesly.houndole@crem.ch>
2019-01-22 14:10:32 +0100

    modify register

--- cm/register_cm.py
+++ cm/register_cm.py
@@ -4,7 +4,7 @@ import logging
 import json
 
 import time
-
+from app.constant import CM_ID
 from app.api_v1.transactions import register
 
 LOG_FORMAT = ('%(levelname) -10s %(asctime)s %(name) -30s %(funcName) '
@@ -16,28 +16,28 @@ LOGGER = logging.getLogger(__name__)
 def start_runner():
     def start_loop():
         not_started = True
-        i = 0
-
         while not_started:
-            if i<=0:
-                LOGGER.info('In start loop')
-                response = register()
-                LOGGER.info("[HTAPI]  register response: %s ", response)
-                LOGGER.info('Server not yet started')
-                i=i+1
-                LOGGER.info('count = %s',str(i))
-                #try:
-                #if
-                json.loads(response)
+            LOGGER.info('In start loop')
+            response = register()
+            LOGGER.info('Server not yet started')
+            cm = json.loads(json.loads(response.decode('utf-8')))
+            cm_id = cm["cm_id"]
+            if str(CM_ID) == str(cm_id) :
                 LOGGER.info('Server started, quiting start_loop')
-                #not_started = False
-                #except :
-
-                #LOGGER.info('Server have not registerd for some reason')
-                time.sleep(2)
+                not_started = False
+            else:
+                LOGGER.info('Server have not registerd for some reason')
+            time.sleep(2)
 
     print('Started runner')
     start_loop()
+
+
+
+if __name__ == '__main__':
+    start_runner()
+
+
 
 
 

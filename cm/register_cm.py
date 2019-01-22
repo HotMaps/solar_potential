fd47eb82829f5d9f2823388d4ea9e7434d3a240c
lesly houndole <lesly.houndole@crem.ch>
2019-01-22 12:19:47 +0100

    test register

--- cm/register_cm.py
+++ cm/register_cm.py
@@ -17,24 +17,28 @@ def start_runner():
     def start_loop():
         not_started = True
         i = 0
+
         while not_started:
-            LOGGER.info('In start loop')
-            response = register()
-            LOGGER.info("[HTAPI]  register response: %s ", response)
-            LOGGER.info('Server not yet started')
-            i=i+1
-            LOGGER.info('count = %s',str(i))
-            try:
+            if i<=0:
+                LOGGER.info('In start loop')
+                response = register()
+                LOGGER.info("[HTAPI]  register response: %s ", response)
+                LOGGER.info('Server not yet started')
+                i=i+1
+                LOGGER.info('count = %s',str(i))
+                #try:
+                #if
                 json.loads(response)
                 LOGGER.info('Server started, quiting start_loop')
-                not_started = False
-            except :
+                #not_started = False
+                #except :
 
-                LOGGER.info('Server not yet started')
+                #LOGGER.info('Server have not registerd for some reason')
                 time.sleep(2)
 
     print('Started runner')
     start_loop()
+
 
 
 

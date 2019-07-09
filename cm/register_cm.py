5fd68a3b69ef24cb4d6df0f1f6d154f99a7e8a7c
lesly houndole <lesly.houndole@crem.ch>
2019-07-09 15:21:57 +0200

    test register 3

--- cm/register_cm.py
+++ cm/register_cm.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
-cpt = 0
 import logging
 
 import json
-import os
+
 import time
 
 from app.api_v1.transactions import register
@@ -12,30 +11,27 @@ LOG_FORMAT = ('%(levelname) -10s %(asctime)s %(name) -30s %(funcName) '
               '-35s %(lineno) -5d: %(message)s')
 LOGGER = logging.getLogger(__name__)
 
-def start_loop():
-    not_started = True
-    i = 0
-
-    LOGGER.info('In start loop')
-    response = register()
-    LOGGER.info("[HTAPI]  register response: %s ", response)
-    LOGGER.info('Server not yet started')
-    i=i+1
-    LOGGER.info('count = %s',str(i))
-
-    json.loads(response)
-    LOGGER.info('Server started, quiting start_loop')
-    not_started = False
-    os.system('kill $PPID')
-
-
-    LOGGER.info('Server not yet started')
-    #time.sleep(2)
-
 
 
 def start_runner():
+    def start_loop():
+        not_started = True
+        i = 0
+        while not_started:
+            LOGGER.info('In start loop')
+            response = register()
+            LOGGER.info("[HTAPI]  register response: %s ", response)
+            LOGGER.info('Server not yet started')
+            i=i+1
+            LOGGER.info('count = %s',str(i))
+            try:
+                json.loads(response)
+                LOGGER.info('Server started, quiting start_loop')
+                not_started = False
+            except :
 
+                LOGGER.info('Server not yet started')
+                time.sleep(2)
 
     print('Started runner 1')
     start_loop()

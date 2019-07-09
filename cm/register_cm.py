65ca55e27fe64f1965029ab269c964a5065fdac8
lesly houndole <lesly.houndole@crem.ch>
2019-07-09 14:58:32 +0200

    test register 3

--- cm/register_cm.py
+++ cm/register_cm.py
@@ -12,36 +12,24 @@ LOG_FORMAT = ('%(levelname) -10s %(asctime)s %(name) -30s %(funcName) '
 LOGGER = logging.getLogger(__name__)
 
 def start_loop():
-    not_started = True
-    i = 0
-    while not_started:
-        LOGGER.info('In start loop')
-        response = register()
-        LOGGER.info("[HTAPI]  register response: %s ", response)
-        LOGGER.info('Server not yet started')
-        i=i+1
-        LOGGER.info('count = %s',str(i))
-        try:
-            json.loads(response)
-            LOGGER.info('Server started, quiting start_loop')
-            not_started = False
-            os.system('kill $PPID')
-        except :
 
-            LOGGER.info('Server not yet started')
-            time.sleep(2)
+    LOGGER.info('In start loop')
+    response = register()
+    LOGGER.info("[HTAPI]  register response: %s ", response)
+    LOGGER.info('Server not yet started')
+    i=i+1
+    LOGGER.info('count = %s',str(i))
+
+    json.loads(response)
+    LOGGER.info('Server started, quiting start_loop')
+    not_started = False
+    os.system('kill $PPID')
 
 
 def start_runner():
 
     print('Started runner')
     start_loop()
-
-
-
-if __name__ == '__main__':
-    start_runner()
-
 
 
 

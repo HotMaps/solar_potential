21136756a3b98b07fdadd8139a459fe9a94008fb
lesly houndole <lesly.houndole@crem.ch>
2019-07-09 14:50:34 +0200

    test register 3

--- cm/app/constant.py
+++ cm/app/constant.py
@@ -14,7 +14,7 @@ PORT_DOCKER = 80
 
 #TODO ********************setup this URL depending on which version you are running***************************
 
-CELERY_BROKER_URL = CELERY_BROKER_URL_DOCKER
+CELERY_BROKER_URL = CELERY_BROKER_URL_LOCAL
 PORT = PORT_DOCKER
 
 #TODO ********************setup this URL depending on which version you are running***************************
--- cm/register_cm.py
+++ cm/register_cm.py
@@ -2,7 +2,7 @@
 import logging
 
 import json
-import sys
+import os
 import time
 
 from app.api_v1.transactions import register
@@ -11,31 +11,31 @@ LOG_FORMAT = ('%(levelname) -10s %(asctime)s %(name) -30s %(funcName) '
               '-35s %(lineno) -5d: %(message)s')
 LOGGER = logging.getLogger(__name__)
 
+def start_loop():
+    not_started = True
+    i = 0
+    while not_started:
+        LOGGER.info('In start loop')
+        response = register()
+        LOGGER.info("[HTAPI]  register response: %s ", response)
+        LOGGER.info('Server not yet started')
+        i=i+1
+        LOGGER.info('count = %s',str(i))
+        try:
+            json.loads(response)
+            LOGGER.info('Server started, quiting start_loop')
+            not_started = False
+            os.system('kill $PPID')
+        except :
+
+            LOGGER.info('Server not yet started')
+            time.sleep(2)
 
 
 def start_runner():
-    def start_loop():
-        not_started = True
-        i = 0
-        while not_started:
-            LOGGER.info('In start loop')
-            response = register()
-            LOGGER.info("[HTAPI]  register response: %s ", response)
-            LOGGER.info('Server not yet started')
-            i=i+1
-            LOGGER.info('count = %s',str(i))
-            try:
-                json.loads(response)
-                LOGGER.info('Server started, quiting start_loop')
-                not_started = False
-                sys.exit()
-            except :
-
-                LOGGER.info('Server not yet started')
-                time.sleep(2)
 
     print('Started runner')
-    #start_loop()
+    start_loop()
 
 
 

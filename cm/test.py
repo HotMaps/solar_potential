73244e789a970f76a837cb88c54eefb77b254285
pietro <peter.zamb@gmail.com>
2019-08-29 13:10:11 +0200

    Remove coverage report because was raising an error

--- cm/test.py
+++ cm/test.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
+import os
 import unittest
 import sys
 import coverage
 
 from tests import suite
 
-COV = coverage.coverage(branch=True, include='cm/app/*')
+
+COV = coverage.coverage(
+        data_file=os.path.join(os.path.dirname(__file__), '.coverage'),
+        branch=True,
+        include='cm/*')
 COV.start()
 
 return_code = not unittest.TextTestRunner(verbosity=2).run(suite).wasSuccessful()
 
 COV.stop()
-COV.report()
+# COV.report()
 
 sys.exit(return_code)
